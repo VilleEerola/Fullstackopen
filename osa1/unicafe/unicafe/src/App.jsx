eab50eafa63392f8d1287eaf04fb872c057ae0a1
Ville Eerola <ville.eerolaa@gmail.com>
2023-09-16 13:40:47 +0300

    tehty tehtävää eteenpäin

--- osa1/unicafe/unicafe/src/App.jsx
+++ osa1/unicafe/unicafe/src/App.jsx
@@ -1,34 +1,110 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Header = (props) => {
+  console.log(props)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <p style = {{fontWeight: "bold", fontSize: 50, textAlign: 'center' }}>{props.header}</p>
+    </div>
+  )
+}
+
+const Header2 = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p style = {{fontWeight: "bold", fontSize: 50, textAlign: 'center' }}>{props.header2}</p>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  console.log(props)
+  return(
+  <div>
+    <p style = {{textAlign: "center"}}>
+      <button onClick={props.handleClick}>
+        {props.text}
+      </button>
+    </p> 
+  </div>
+  )
+}
+
+const Statistics = (props) => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+ 
+  const all = (good + neutral + bad)
+  const average = (good*1 + neutral*0 + bad*(-1)) / all
+  const positive = ((good / all) * 100)
+
+  const feedbackGiven = all > 0;
+  console.log(props)
+  return (
+    <div>
+    {feedbackGiven ? (
+      <>
+        <Statistics text = "Good" stats = {good}/>
+        <Statistics text = "Neutral" stats = {neutral}/>
+        <Statistics text = "Bad" stats = {bad}/>
+        <Statistics text = "All" stats = {all}/>
+        <Statistics text = "Average" stats = {average}/>
+        <Statistics text = "Positive %" stats = {positive}/>
+      </>
+    ):(
+      <p style={{textAlign: "center"}}>No Feedback Given</p>
+    )}
+    </div>
+  )
+}
+
+const StatisticsLine = (props) => {
+  console.log(props)
+  return (
+    <p style={{textAlign:"center"}}>{props.text} {props.stats}</p>
+  )
+}
+
+
+
+const App = () => {
+  const header = "Give Feedback"
+  const header2 = "Statistics"
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = (newValue) => {
+    console.log('good now', newValue)
+    setGood(newValue)
+  }
+
+  const setToNeutral = (newValue) => {
+    console.log('neutral now', newValue)
+    setNeutral(newValue)
+  }
+
+  const setToBad = (newValue) => {
+    console.log('bad now', newValue)
+    setBad(newValue)
+  }
+  
+  return (
+    <div>
+      <Header header = {header}/>
+
+      <Button handleClick = {() => setToGood(good + 1)} text="Good"/>
+      <Button handleClick = {() => setToNeutral(neutral + 1)} text="Neutral"/>
+      <Button handleClick = {() => setToBad(bad + 1)} text="Bad"/>
+
+      <Header2 header2 = {header2}/>
+        <Statistics />
+    </div>
   )
 }
 
